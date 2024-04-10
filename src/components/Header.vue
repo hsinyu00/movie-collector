8457d84b90204fa3480885731e344446e35fc2a5
Hsinyu Chou <hsinyuchou@melaleuca.com>
2024-04-10 15:51:05 +0800

    Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,100 @@
+<script>
+//   const loginUser = JSON.parse(localStorage.getItem('login-user')); 
+
+  export default {
+    data() {
+        return {
+            loginUser: JSON.parse(localStorage.getItem('login-user')),
+        }
+    },
+    methods: {
+        logout() {
+            this.loginUser = '';
+            localStorage.removeItem("login-user");
+        }
+    }
+  }
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <nav>
+        <p> {{loginUser.name}} 的電影間</p>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/Signup">Sign up</RouterLink>
+        <RouterLink to="/Signin">Sign in</RouterLink>
+        <a @click="logout">Logout</a>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+ 
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
