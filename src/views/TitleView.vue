02665bfec4640b9eb7d038f55013b88084e1f2e3
Hsinyu Chou <hsinyuchou@melaleuca.com>
2024-04-25 17:47:15 +0800

    add expected TitleView

---
+++ src/views/TitleView.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- <h1>{{ item.title }}</h1>
+    <img :src="item.imgPath" alt="">
+    <p>{{ item.year }}</p> -->
+    {{ $route.params.id }}
+</template>
+
+<script setup>
+    
+</script>
+
+<style scoped>
+
+</style>
