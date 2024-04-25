0c7dd21affb46284943ce0a09208b4019d1c2440
Hsinyu Chou <hsinyuchou@melaleuca.com>
2024-04-25 14:51:44 +0800

    use defineProps in ListItem

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,17 +1,26 @@
 <template>
-    <router-link :to="`/title/${item.id}`">
+    <router-link :to="{ path: `/title/${props.item.id}`}">
         <div class="list-item">
-            <img :src="item.imgPath" alt="">
-            <h2>{{ item.title }}</h2>
-            <p>{{ item.year }}</p>
+            <img :src="props.item.imgPath" alt="">
+            <h2>{{ props.item.title }}</h2>
+            <p>{{ props.item.year }}</p>
         </div>
     </router-link>
 </template>
 
-<script>
+<!-- <script>
     export default {
         props: ['item'],
     }
+</script> -->
+
+<script setup>
+const props = defineProps({
+  'item': {
+    type: Object,
+    required: true
+  }
+})
 </script>
 
 <style scoped>
