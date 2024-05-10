d1a0313a01b79c363ab386c7aef0ca2d167845c0
Hsinyu Chou <hsinyuchou@melaleuca.com>
2024-05-10 18:23:01 +0800

    pass id as a param and get detail data by axio

--- src/views/TitleView.vue
+++ src/views/TitleView.vue
@@ -1,17 +1,47 @@
 <template>
-    <!-- <h1>{{ item.title }}</h1>
-    <img :src="item.imgPath" alt="">
-    <p>{{ item.year }}</p> -->
-    {{ id }}
+    <h1>{{ titleInfo.title }}</h1>
+    <img :src="titleInfo.imgPath" >
+    <p>Year {{ titleInfo.year }}</p>
+    {{ titleInfo.id }}
 </template>
 
 <script setup>
+import { ref, onMounted, toRefs } from 'vue'
+import axios from 'axios'
+
 const props = defineProps({
   id: {
     type: String,
     required: true
   }
 })
+
+const { id } = toRefs(props);
+let titleInfo = ref({})
+
+// console.log('id.value', id.value);
+
+const getTitleData = async () => {
+    axios.get("http://localhost:8000/topicData")
+    .then((res)=>{
+        for (let i = 0; i < res.data.length; i++) {
+          console.log(res.data[i].titles)
+          let titleObj = res.data[i].titles.filter((title) => title.id === id.value)
+          if(titleObj.length === 1) {
+            // console.log('titleObj[0]', titleObj[0])
+            titleInfo.value = titleObj[0]
+          }
+        }
+    })
+    .catch((err)=> {
+        console.log(err);
+    })
+}
+
+onMounted(() => {
+  getTitleData()
+})
+
 </script>
 
 <style scoped>
